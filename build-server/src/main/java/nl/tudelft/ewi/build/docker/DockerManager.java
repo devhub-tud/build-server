6302af3d264577cd113c2f271628e73dffb55450
Michael de Jong <michaelj@minicom.nl>
2014-04-09 15:05:06 +0200

    Builds now return correct exit code.

--- build-server/src/main/java/nl/tudelft/ewi/build/docker/DockerManager.java
+++ build-server/src/main/java/nl/tudelft/ewi/build/docker/DockerManager.java
@@ -51,7 +51,7 @@ public class DockerManager {
 				logFuture.cancel(true);
 				
 				stop(host, container);
-				//delete(host, container);
+				delete(host, container);
 				logger.onClose(code.getStatusCode());
 			}
 		});
